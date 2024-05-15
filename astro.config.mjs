42b6008f82a04c0522a9f415629712c05beb7048
Ritz <ritz.exists@gmail.com>
2024-05-15 14:23:32 -0700

    added fixes to hopefully make compilation happen

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 
-import sitemap from '@astrojs/sitemap';
-
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://pwn.nz',
 	integrations: [mdx(
 		{
 			extendMarkdownConfig: false,
 			smartypants: true,
 			gfm: true,
 		}
-	), sitemap()],
+	)],
 });
